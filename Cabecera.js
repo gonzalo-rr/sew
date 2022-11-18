f3913e90a52a15bfcc0b015854ff9098252e6079
gonzalo-rr <61070475+gonzalo-rr@users.noreply.github.com>
2022-11-18 10:57:07 +0100

    Add files via upload

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y Estándares para la Web (SEW)";
+asignatura.titulacion = "Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = 3;
+asignatura.estudiante = "Gonzalo";
+asignatura.email = "uo283874@uniovi.es";
---
+++ Parrafos.js
@@ -0,0 +1,9 @@
+document.write("<p>");
+document.write("Curso: " + asignatura.curso);
+document.write("</p>");
+document.write("<p>");
+document.write("Estudiante: " + asignatura.estudiante);
+document.write("</p>");
+document.write("<p>");
+document.write("Email: " + asignatura.email);
+document.write("</p>");
