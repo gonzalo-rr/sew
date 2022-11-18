130bf26b7ee742cf7ef67aa55ac69e290b182875
gonzalo-rr <61070475+gonzalo-rr@users.noreply.github.com>
2022-11-18 11:39:52 +0100

    Delete Parrafos.js

--- Parrafos.js
+++
@@ -1,9 +0,0 @@
-document.write("<p>");
-document.write("Curso: " + asignatura.curso);
-document.write("</p>");
-document.write("<p>");
-document.write("Estudiante: " + asignatura.estudiante);
-document.write("</p>");
-document.write("<p>");
-document.write("Email: " + asignatura.email);
-document.write("</p>");
