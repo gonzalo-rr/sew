ade3c4eb2e9ae007542a21a12f054f27d0e055ae
gonzalo-rr <61070475+gonzalo-rr@users.noreply.github.com>
2022-11-21 22:20:51 +0100

    Delete InfoNavegador.js

--- InfoNavegador.js
+++
@@ -1,5 +0,0 @@
-var navegador = new Object();
-navegador.nombre = navigator.appName;
-navegador.idioma = navigator.language;
-navegador.localizacion = navigator.geolocation.getCurrentPosition();
-navegador.cookies = navigator.cookieEnabled;
