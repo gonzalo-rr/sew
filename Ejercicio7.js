5dca5cceece8020e4a02b73a1aa1d9812561f70b
gonzalo-rr <61070475+gonzalo-rr@users.noreply.github.com>
2022-12-06 12:12:38 +0100

    Add files via upload

---
+++ Ejercicio7.js
@@ -0,0 +1,66 @@
+class QHelper {
+    constructor () {
+        this.tituloOriginal = true;
+    }
+    mostrar() {
+        $("li").show();
+    }
+    ocultar() {
+        $("li").hide();
+    }
+    cambiarTitulo() {
+        if (this.tituloOriginal) {
+            $("h1").text("Ejercicio 1 Segunda Parte ECMAScript");
+            this.tituloOriginal = false;
+        }
+        else {
+            $("h1").text("Ejercicio 7 JQuery");
+            this.tituloOriginal = true;
+        }
+    }
+    añadirContenido() {
+        var h2 = $("<h2></h2>").text("ECMA Script");
+        var p1 = $("<p></p>").text("ECMAScript es una especificación de lenguaje de programación publicada por Ecma International. El desarrollo empezó en 1996 y estuvo basado en el popular lenguaje JavaScript propuesto como estándar por Netscape Communications Corporation. Actualmente está aceptado como el estándar ISO/IEC 22275:2018.");
+        var p2 = $("<p></p>").text("ECMAScript define un lenguaje de tipos dinámicos ligeramente inspirado en Java y otros lenguajes del estilo de C. Soporta algunas características de la programación orientada a objetos mediante objetos basados en prototipos y pseudoclases.");
+        var p3 = $("<p></p>").text("La mayoría de navegadores de Internet incluyen una implementación del estándar ECMAScript, al igual que un acceso al Document Object Model para manipular páginas web. JavaScript está implementado en la mayoría de navegadores, Internet Explorer de Microsoft usa JScript. El navegador Opera tenía su propio intérprete de ECMAScript con extensiones para soportar algunas características de JavaScript y JScript, actualmente Opera está basado en Chromium (y utiliza su intérprete). Cada navegador tiene extensiones propias al estándar ECMAScript, pero cualquier código que se adecúe al estándar debería funcionar en todos ellos.");
+        $("main").last().after(p1);
+        $("p").last().before(h2);
+        $("p").last().after(p2);
+        $("p").last().after(p3);
+    }
+    eliminarContenido() {
+        $("h2").remove();
+        $("p").remove();
+        $("ul").remove();
+        $("li").remove();
+    }
+    mostrarRecorrido() {
+        $("*", document.body).each(function() {
+            var etiquetaPadre = $(this).parent().get(0).tagName;
+            $("article").last().prepend(document.createTextNode( "Etiqueta padre : &lt;"  + etiquetaPadre + "&gt; elemento : &lt;" + $(this).get(0).tagName +"&gt; valor: "));
+        });
+    }
+    sumarTabla() {
+        var c = 0;
+        var sumsCols = [0, 0, 0];
+        var sumsRows = [0, 0, 0];
+        var total = 0;
+        $("table tr td").each(function() {
+            sumsRows[Math.floor(c / 3)] += new Number(this.innerHTML);
+            sumsCols[Math.floor(c % 3)] += new Number(this.innerHTML);
+            total += new Number(this.innerHTML);
+            c++;
+        });
+        var i = -1;
+        $("table tr").each(function() {
+            if (i >= 0) {
+                $(this).prepend($("<td></td>").text(sumsRows[i]));
+            } else {
+                $(this).prepend($("<th></th>").text("suma"));
+            }
+            i++;
+        });
+        $("tbody").prepend("<tr>\n\t<th>suma</th>\n\t<td>," + sumsCols[0] + "</td>\n\t<td>," + sumsCols[1] + "</td>\n\t<td>," + sumsCols[2] + "</td>\n\t<td>," + total + "</td></tr>");
+    }
+}
+var helper = new QHelper();
